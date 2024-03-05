220985005cfaaec653c51f26022ccc1d669106a3
Flakon500 <benedek.games@gmail.com>
2024-03-05 11:11:32 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+#import plotly.express as px
 
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 import math
+#a=1;
+#print(a)
