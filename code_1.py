4812e2607bcf9ceba29f38cab691de753184f805
Nemesaaa <65776008+Nemesaaa@users.noreply.github.com>
2024-04-21 17:08:37 +0200

    Nekem baszakodott a név miatt

---
+++ code_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import plotly.express as px
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+import math
+#a=1;
+#print(a)
