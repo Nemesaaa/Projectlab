cbd7ff5aad1b7df5b92f35cc03023b66f1c2016f
Nemesaaa <65776008+Nemesaaa@users.noreply.github.com>
2024-04-22 22:22:48 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+
+# Load the XML data from a file
+xml_file = "gyor_forg_15_min.xml"
+
+# Parse the XML
+tree = ET.parse(xml_file)
+root = tree.getroot()
+
+# Accessing interval attributes
+intervals = root.findall('.//interval')
+for interval in intervals:
+    interval_id = interval.attrib['begin']
+    print("Begin:", interval_id)
+    
+    # Accessing edge attributes
+    edges = interval.findall('.//edge')
+    for edge in edges:
+        edge_id = edge.attrib['id']
+        left_value = edge.attrib['left']
+        print("Edge ID:", edge_id)
+        print("Left Value:", left_value)
+
+    break
