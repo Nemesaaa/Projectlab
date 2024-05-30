e80332a9ed6735196c7a75c48f2a1f68bd66a15a
Nemesaaa <65776008+Nemesaaa@users.noreply.github.com>
2024-05-30 22:42:53 +0200

    lista készítő kód 80%, még a datat felkell tölteni

---
+++ generate/generated_files/list_maker_inner.py
@@ -0,0 +1,121 @@
+import sumolib
+import networkx as nx
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import random
+
+import torch
+import torch_geometric
+import torch_geometric.data as Data
+import torch_geometric.utils as pyg_utils
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+import numpy as np
+
+from os import walk
+import os
+import xml.etree.cElementTree as et
+
+f = []
+
+#Function to read in the network we work with into a networkx object with the nodes and edges, no features yet
+def read_sumo_net(filename):
+    net = sumolib.net.readNet(filename)
+    G = nx.Graph()
+    
+    # Add nodes
+    for node in net.getNodes():
+        G.add_node(node.getID(), pos=(node.getCoord()))
+    # Add edges
+    for edge in net.getEdges():
+        for lane in edge.getLanes():
+            edge_id = lane.getEdge().getID()
+            starting_node_id = net.getEdge(edge_id).getFromNode().getID()
+            ending_node_id = net.getEdge(edge_id).getToNode().getID()
+            G.add_edge(starting_node_id, ending_node_id, edge_id = edge_id)
+    return G
+
+#Function to add the features to the network graph we created already
+
+def add_edge_features_from_xml(G, xml_filename, interval_begin):
+    # Parse the XML file
+    tree = ET.parse(xml_filename)
+    root = tree.getroot()
+    #Find the interval corresponding to the interval_begin time 
+    interval = root.find(f'.//interval[@begin="{interval_begin}"]')
+    #Extract all the features of the edges
+    edges = interval.findall('.//edge')
+    for edge in edges:
+        edge_id = edge.get('id')
+        edge_features = {}
+        edge_features['left'] = edge.get('left')
+        #We can add other features here
+        #Iterate through the edges in the existing NetworkX graph
+        for xml_edge_id, xml_edge_data in G.edges.items():
+            if G.get_edge_data(xml_edge_id[0],xml_edge_id[1])['edge_id'] == edge_id:
+                G.edges[xml_edge_id].update(edge_features)
+    return G
+
+def nx_to_pyg(graph):
+    # Convert NetworkX graph to PyTorch Geometric Data object
+    pyg_data = Data.Data()
+    #We have to number the nodes, because that is how Data object works
+    # Mapping between string node IDs and numerical indices
+    node_id_to_index = {node_id: i for i, node_id in enumerate(graph.nodes)}
+
+    # Set node features
+    num_nodes = graph.number_of_nodes()
+    node_features = np.zeros((num_nodes, 2))  # Assuming num_features is known, this is important to change, if we want to change something, altough I do not think that will be the case for us
+    for i, (node, features) in enumerate(graph.nodes(data=True)):
+        node_features[i] = [features['pos'][0], features['pos'][1]]  # Add node features accordingly, this case the coordinates
+    pyg_data.x = torch.tensor(node_features, dtype=torch.float)
+
+    # Set edge features and edge indices
+    edge_index = []
+    edge_features = []
+    for u, v, features in graph.edges(data=True):
+        # Map string node IDs to numerical indices
+        u_index = node_id_to_index[u]
+        v_index = node_id_to_index[v]
+        edge_index.append([u_index, v_index])
+        edge_features.append([float(features['left'])])  # Add edge features accordingly, if we add more features, we have to change this line
+
+    pyg_data.edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+    pyg_data.edge_attr = torch.tensor(edge_features, dtype=torch.float)
+
+    return pyg_data
+
+
+#Function to plot the graph
+def plot_graph(G):
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, pos, with_labels=False, node_size=10)
+    plt.show()
+Graph = read_sumo_net('s_gyor.net.xml')
+
+G1 = read_sumo_net('s_gyor.net.xml')
+G2 = add_edge_features_from_xml(G1,'gyor_forg_15_min_1_1.xml',"0.00")
+pyg_data = nx_to_pyg(G2)
+#print(pyg_data)
+
+f = []
+mypath=os.getcwd()
+for (dirpath, dirnames, filenames) in walk(mypath):
+
+    f.extend(filenames)
+    break
+for i in f:
+    splited = i.split(".")
+    if splited[1] !="xml":
+        f.remove(i)
+f.remove("s_gyor.net.xml")
+print(f)
+
+timesteps=[]
+kezd = 1800
+for i in range(14):
+    timesteps.append(str(kezd+i*900)+".00")
+
+
